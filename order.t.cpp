12aa206377f21e1ad7dba9505ada98bfea7b02f2
CBerger1997 <callumberger@hotmail.com>
2018-10-26 09:15:58 +0100

    Added more tests for order and orderbook

--- order.t.cpp
+++ order.t.cpp
@@ -26,3 +26,51 @@ TEST(Serialise, ValidInputSell) {
 	
     EXPECT_EQ("BT.A S 300 54.67 1", newOrder.serialise());
 }
+
+TEST(IsBuy, ValidReturnBuy) {
+    Order newOrder( "BT.A", Order::Buy, 300, 54.67, 1 );
+	
+    EXPECT_EQ(true, newOrder.isBuy());
+}
+
+TEST(IsBuy, ValidReturnSell) {
+    Order newOrder( "BT.A", Order::Sell, 300, 54.67, 1 );
+	
+    EXPECT_EQ(false, newOrder.isBuy());
+}
+
+TEST(ClientId, ValidValue) {
+    Order newOrder( "BT.A", Order::Buy, 300, 54.67, 1 );
+	
+    EXPECT_EQ(1, newOrder.clientId());
+}
+
+TEST(ClientId, ZeroValue) {
+    Order newOrder( "BT.A", Order::Buy, 300, 54.67, 0 );
+	
+    EXPECT_EQ(0, newOrder.clientId());
+}
+
+TEST(ClientId, NegativeValue) {
+    Order newOrder( "BT.A", Order::Buy, 300, 54.67, -1 );
+	
+    EXPECT_EQ(-1, newOrder.clientId());
+}
+
+TEST(LimitPrice, ValidValue) {
+    Order newOrder( "BT.A", Order::Buy, 300, 54.67, 1 );
+	
+    EXPECT_EQ(54.67, newOrder.getLimitPrice());
+}
+
+TEST(LimitPrice, ZeroValue) {
+    Order newOrder( "BT.A", Order::Buy, 300, 0.0, 1 );
+	
+    EXPECT_EQ(0.0, newOrder.getLimitPrice());
+}
+
+TEST(LimitPrice, NegativeValue) {
+    Order newOrder( "BT.A", Order::Buy, 300, -54.67, 1 );
+	
+    EXPECT_EQ(-54.67, newOrder.getLimitPrice());
+}
--- orderbook.t.cpp
+++ orderbook.t.cpp
@@ -1,11 +1,22 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
 #include "orderbook.h"
+#include "orderqueue.h"
 
-TEST (OrderBookTest, test1)
+TEST (Constructor, ValidInput)
 {
-    OrderBook b;
-	EXPECT_EQ(1,1);
+    OrderQueue buy;
+    OrderQueue sell;
+    
+    Order newOrder1( "BT.A", Order::Buy, 300, 54.67, 1 );
+    Order newOrder2( "BT.A", Order::Sell, 300, 54.67, 1 );
+    
+    buy.insert(newOrder1);
+    sell.insert(newOrder2);
+    
+    OrderBook book(buy, sell);
+	
+    // TODO: FIND A WAY TO TEST THIS
 }
 
 TEST (OrderBookTest, test2)
